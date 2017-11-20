fbe036a89061a63400e2b579ef6293b67e89854f
Michael Pope <popemray1986@gmail.com>
2017-11-20 03:35:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,8 +37,8 @@ class Song
     self.find_by_name(name) || self.create_by_name(name)
   end
 
-  def self.alphabetical
-    self.sort_by {|song| song.name}
+  def self.alphabetical(name)
+    self.sort_by {|song| song.name }
 
   end
 
